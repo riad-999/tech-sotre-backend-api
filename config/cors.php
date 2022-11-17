6bc575174a65d4ca0bd302484398730019caaeff
Riad <felihriad5@gmail.com>
2022-11-17 08:34:49 +0100

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://pc-store.vercel.app'],
+    'allowed_origins' => ['https://pc-store.vercel.app', 'https://pc-store.riadfelih.com'],
 
     'allowed_origins_patterns' => [],
 
